e470895ad74a73dcd236c75cbf0059b13bc5e435
juseus03 <juseus@gmail.com>
2016-03-15 13:50:41 -0500

    Agrega La aplicación base
    Proyecto basado en bluetoothchat

---
+++ WirellesNB_temporal/Application/src/main/java/com/example/android/Util/Util.java
@@ -0,0 +1,9 @@
+package com.example.android.Util;
+
+/**
+ * Created by Juan Useche on 7/03/2016.
+ */
+public interface Util {
+    public static final boolean DEBUG = true;
+    public static final String LOG_TAG = "gles20tut";
+}
---
+++ WirellesNB_temporal/Application/src/main/java/com/example/android/bluetoothchat/IniciarJuegoActividad.java
@@ -0,0 +1,68 @@
+package com.example.android.bluetoothchat;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class IniciarJuegoActividad extends Activity {
+    /** Hold a reference to our GLSurfaceView */
+    private GLSurfaceView mGLSurfaceView;
+    boolean rendererSet;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        mGLSurfaceView = new GLSurfaceView(this);
+
+        // Check if the system supports OpenGL ES 2.0.
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+
+        if (supportsEs2)
+        {
+            // Request an OpenGL ES 2.0 compatible context.
+            mGLSurfaceView.setEGLContextClientVersion(2);
+
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView.setRenderer(new JuegoRender());
+            rendererSet = true;
+        }
+        else
+        {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return;
+        }
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+
+}
